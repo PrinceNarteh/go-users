ad809ceb9676a1888da306ee4b462018f85fd077
Prince Narteh <prinarteh@gmail.com>
2023-12-23 05:22:50 +0000

    feat: configure environment variables

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Working!!!")
+	// loading environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// creating fiber instance
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(http.StatusOK).JSON(&fiber.Map{"message": "API running"})
+	})
+
+	app.Listen(":4000")
 }
